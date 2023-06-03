1d67d2b0743276f55afbab63e9871dbb5fc8031d
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2023-06-02 21:00:55 -0400

    Add files via upload

--- Driver_Install.ps1
+++ Driver_Install.ps1
@@ -1,19 +1,25 @@
 $MODEL = Get-CimInstance -ClassName Win32_BIOS | Select-Object Manufacturer;
 if($MODEL -like '*Dell*') {
 #Install Dell Command Update
+Write-Host "Installing Dell Command Update"
 %programdata%\chocolatey\choco.exe install dellcommandupdate-uwp -y
+
+#Setting up Parameters for DCU
+Write-Host "Configuring Dell Command Update"
 
 $dcuCliPath = "${env:ProgramFiles}\Dell\CommandUpdate\dcu-cli.exe"
 $arguments = '/configure', '-userConsent=disable', '-lockSettings=enable', '-autoSuspendBitLocker=enable', '-silent', '-scheduleWeekly=Mon,16:45', '-scheduleAction=DownloadInstallAndNotify'
 Start-Process -FilePath $dcuCliPath -ArgumentList $arguments -Wait
 
-
 if (Test-Path -Path "HKLM:\SOFTWARE\Dell\UpdateService\Clients\CommandUpdate\Preferences\CFG") {[void](New-ItemProperty -Path "HKLM:\SOFTWARE\Dell\UpdateService\Clients\CommandUpdate\Preferences\CFG" -Name "ShowSetupPopup" -Value 0 -PropertyType DWord -Force)}
 
+
 } elseif ($MODEL -like '*Lenovo*') {
+Write-Host "Installing Lenovo System Update"
 #Install Lenovo System Update
 %programdata%\chocolatey\choco.exe install lenovo-thinkvantage-system-update -y
 
+Write-Host "Configuring Lenovo System Update"
 # Write to HKLM\SOFTWARE\WOW6432Node\Lenovo\System Update\Preferences\UserSettings\General\MetricsEnabled value NO, to disable metrics
 if (Test-Path -Path "HKLM:\SOFTWARE\WOW6432Node\Lenovo\System Update\Preferences\UserSettings\General") {[void](New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Lenovo\System Update\Preferences\UserSettings\General" -Name "MetricsEnabled" -Value "NO" -PropertyType String -Force)}
 
@@ -28,3 +34,7 @@ if (Test-Path -Path "HKLM:\SOFTWARE\WOW6432Node\Lenovo\System Update\Preferences
     Write-Host "This isn't an Dell, or Lenovo Device"
 
 }
+
+
+Write-Host "Installing Windows Updates"
+Get-WindowsUpdate -Install -AcceptAll -AutoReboot
