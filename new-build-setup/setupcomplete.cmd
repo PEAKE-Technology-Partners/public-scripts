82d88b32346682aacbb1b2ac0483bb6c981497b2
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2023-06-23 20:00:41 -0400

    Update setupcomplete.cmd

--- new-build-setup/setupcomplete.cmd
+++ new-build-setup/setupcomplete.cmd
@@ -3,10 +3,7 @@ set SCRIPT_URL=https://raw.githubusercontent.com/PEAKE-Technology-Partners/publi
 set SCRIPT_FILE=%TEMP%\drivers.ps1
 
 REM Download the PowerShell script
-powershell -Command "$webClient = New-Object Net.WebClient; $webClient.DownloadProgressChanged += { Write-Progress -Activity 'Script Download' -Status ('Downloading {0}' -f $_.FileName) -PercentComplete $_.ProgressPercentage }; $webClient.DownloadFileAsync('%SCRIPT_URL%', '%SCRIPT_FILE%')"
-
-REM Wait for the script download to complete
-powershell -Command "while (Test-Path -Path '%SCRIPT_FILE%.part') { Start-Sleep -Seconds 1 }"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%SCRIPT_URL%', '%SCRIPT_FILE%')"
 
 REM Execute the downloaded PowerShell script
 powershell -ExecutionPolicy Bypass -File "%SCRIPT_FILE%"
